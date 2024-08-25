6229154b4e0ee930ce7f55622e7835a2de2bacf4
Sanjaypal1916 <sanjaypal606060@gmail.com>
2024-08-25 20:25:59 +0530

    added OpenAPI Documentation

---
+++ productservice/productservice/src/main/java/com/dockermicroservices/productservice/config/OpenAPIConfig.java
@@ -0,0 +1,27 @@
+package com.dockermicroservices.productservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenAPIConfig {
+	
+//	upgrading the OpenAPI Documentation 
+	
+	@Bean
+	public OpenAPI productServiceAPI() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("product Service API")
+						.description("this is the REST API for product service")
+						.version("v0.0.1")
+						.license(new License().name("Apache 2.0")))
+				.externalDocs(new ExternalDocumentation()
+						.url("https://product-service-dummy-url.com/docs"));
+	}
+}
